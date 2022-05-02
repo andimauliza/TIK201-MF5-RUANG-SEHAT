05a234088b2a05da130b412ec3ba1918fda992bf
Reza Fahlevi <reza.f20@mhs.unsyiah.ac.id>
2022-05-03 01:30:33 +0700

    Mengkoneksikan ke database untuk menyajikan data ke halaman user

--- detailroom.php
+++ detailroom.php
@@ -1,3 +1,21 @@
+<?php 
+// komeksi ke databas
+$conn = mysqli_connect("localhost", "root", "", "ruang_sehat");
+
+// ambil data / query dari tabel kamar
+// $result = mysqli_query($conn, "SELECT * FROM kamar");
+
+// $row = mysqli_fetch_assoc($result);
+
+$sql    = "SELECT * FROM tb_ruangan";
+$result = $conn->query($sql);
+
+$id_kamar           = $_GET['id_kamar'];
+$tb_ruangan           = "SELECT * FROM tb_ruangan WHERE id_kamar='$id_kamar'";
+$result_tb_ruangan    = $conn->query($tb_ruangan);
+$rows                = $result_tb_ruangan->fetch_array();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,25 +55,28 @@
     <!-- content -->
     <section class="content">
         <div class="hero">
-            <img src="img/hero.png" alt="">
+            <img src="img/<?= $rows["image_kamar"] ?>" alt="">
         </div>
         <div class="data">
-            <h4>Kamar Mawar 1</h4>
-            <p>VIP</p>
-            <p class="badge bg-success">Full</p>
+            <h4><?= $rows["nama_ruangan"] ?>"</h4>
+            <p><?= $rows["kategori"] ?>"</p>
+            <p class="badge bg-success">Available</p>
             <h4 class="fasilitas">Fasilitas</h4>
             <div class="row mt-3">
                 <div class="col text-center">
                     <img src="img/tv.png" alt="" class="mb-1">
-                    <h5>TV Plasma</h5>
+                    <h5>TV</h5>
+                    <span><?= $rows["tv"] ?></span>
                 </div>
                 <div class="col text-center">
                     <img src="img/lemari.png" alt="" class="mb-1">
                     <h5>Lemari Pakaian</h5>
+                    <span>Tidak Tersedia</span>
                 </div>
                 <div class="col text-center">
                     <img src="img/oksigen.png" alt="" class="mb-1">
                     <h5>Oksigen Sentral</h5>
+                    <span>Tidak Tersedia</span>
                 </div>
                 
             </div>
@@ -63,14 +84,17 @@
                 <div class="col text-center">
                     <img src="img/kamarmandi.png" alt="" class="mb-1">
                     <h5>Kamar Mandi</h5>
+                    <span><?= $rows['kamar_mandi'] ?></span>
                 </div>
                 <div class="col text-center">
                     <img src="img/ac.png" alt="" class="mb-1">
                     <h5>AC</h5>
+                    <span><?= $rows['ac'] ?></span>
                 </div>
                 <div class="col text-center">
                     <img src="img/meja.png" alt="" class="mb-1">
                     <h5>Meja Makan</h5>
+                    <span><?= $rows['meja_makan'] ?></span>
                 </div>
             </div>
         </div>
